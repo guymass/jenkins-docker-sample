ccfea7320852ad034a35cdad2dd16f3bfeca5a16
guymass <guymass3@gmail.com>
2022-09-14 14:46:36 +0300

    adding test.java file

---
+++ src/main/java/massdo/test.java
@@ -0,0 +1,8 @@
+package massdo;
+
+public class test {
+    
+    public static void main(String[] args) {
+        System.out.println("Starting the application. Welcome! \n ###################################")
+    }
+}
